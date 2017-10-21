095ba4eebc668b2b34fde0fcccfa42c301392e61
uri <uri.valevski@gmail.com>
2017-10-21 03:46:06 +0300

    simplify a bit

--- places.js
+++ places.js
@@ -7,6 +7,8 @@ exports.init = function() {
   TOKEN = require('./tokens').tokens.gmaps;
 };
 
+// Prepare a link with all needed details to call the places API.
+// We sometimes want just the URL for future requests, so this function doesn't perform the call itself.
 const makeUrlWithParams = function(action, params, parseJson) {
   const searchParams = ['key=' + TOKEN];
   Object.keys(params).forEach(k => {
@@ -19,6 +21,7 @@ const makeUrlWithParams = function(action, params, parseJson) {
     : ''}?${searchParams.join('&')}`;
 };
 
+// Prepare a link, do the request and return the parsed response.
 const doRequest = function(action, params, parseJson = true) {
   return fetch(makeUrlWithParams(action, params, parseJson), {
     method: 'GET',
@@ -38,6 +41,7 @@ const doRequest = function(action, params, parseJson = true) {
   });
 };
 
+// Makes a URL for a picture for the given result.
 // Response example:
 // https://maps.googleapis.com/maps/api/place/photo?key=API_KEY&maxwidth=400&photoreference=CnRtAAAATLZNl354RwP_9UKbQ_5Psy40texXePv4oAlgP4qNEkdIrkyse7rPXYGd9D_Uj1rVsQdWT4oRz4QrYAJNpFX7rzqqMlZw2h2E2y5IKMUZ7ouD_SlcHxYq1yL4KbKUv3qtWgTK0A6QbGh87GB3sscrHRIQiG2RrmU_jF4tENr9wGS_YxoUSSDrYjWmrNfeEHSGSc3FyhNLlBU
 const getPictureSource = (result, maxwidth = 500) =>
@@ -49,6 +53,9 @@ const getPictureSource = (result, maxwidth = 500) =>
       )
     : undefined;
 
+// Given a Google Maps response and location, returns a nice description string with:
+// - current open status
+// - distance
 const createDescription = (result, location) => {
   let ret = !result.opening_hours
     ? 'Opening hours unknown'
@@ -64,26 +71,26 @@ const createDescription = (result, location) => {
   return ret;
 };
 
+// A cached data requester based on place ID.
 const cachedPlaceInfo = makeCachedFunction(doRequest, 'placeInfoDetails');
 
 // Response example:
 // https://maps.googleapis.com/maps/api/place/details/json?key=API_KEY&placeid=ChIJEScAUn9MHRURzG4ggK8ID4Y
 exports.placeInfo = async placeid => {
-  const currentLocation = geolocate.get();
-  const placeInfoResponse = await cachedPlaceInfo('details', { placeid });
-  if (!placeInfoResponse.result) {
+  const { result } = await cachedPlaceInfo('details', { placeid });
+  if (!result) {
     return Promise.reject();
   }
-  placeInfo = placeInfoResponse.result;
+  const currentLocation = geolocate.get();
   return {
     id: placeid,
-    title: placeInfo.name,
+    title: result.name,
     category: 'restaurants',
-    link: placeInfo.url,
-    description: createDescription(placeInfo, currentLocation),
+    link: result.url,
     locationComplete: currentLocation.coords,
-    bigPictureSource: getPictureSource(placeInfo),
-    smallPictureSource: getPictureSource(placeInfo, 100)
+    description: createDescription(result, currentLocation),
+    bigPictureSource: getPictureSource(result),
+    smallPictureSource: getPictureSource(result, 100)
   };
 };
 
@@ -123,41 +130,37 @@ const innerSearchPlace = async (query, location) => {
 exports.searchPlace = async query =>
   mapSearchResultsToEntities(await innerSearchPlace(query, getLocationParam()));
 
-const getResultsWithNextPageFunction = (
-  response,
-  existingEntities,
-  params
-) => ({
+// Returns an object with processed results, filtered against excludedPlaces and a recursion call for next pages.
+// Example URL for next_page_token usage:
+// https://maps.googleapis.com/maps/api/place/textsearch/json?key=API_KEY_HERE&location=32.0853%2C34.7818&type=restaurant&pagetoken=CpQCBwEAAGjYNql6iKPC2kOaCtS4Lc4tyj42ak9WZy2h6isCJWEUY9TQUpAh1Vwf35M0MC2qf9l1zJVgrnEqIt5GPYaM3dw2EiwgM1EEr--upS0FoKvv94jN3ACJVYTmrgA0SzguL-Np4AkJ704ngw1wKfdMaWvbvjVEiBlA3OUtPY3-zrichl-JaMerl0d_1FHzyRnyxBYWuKEEG-3S6FjaQOh-0Ks4lekJZiYW6NhD2QX3OVDFMNlpGs-88j-_3KT4AnUqzoC6KjUnE4GCY2-BdTGPW6hmNCXq2XIYtpsQ3xquT7iJb_C5N1lOSZebR-DLbwrDS7zkw_yDgapqr-yoeqVK0Jt9tBEeHGups2fnaovL1CEkEhDfhtVHsmK1rSaqt9sHrELRGhToqlUePY-ABMZgIprHyJtxaKTFNg
+const getResultsWithNextPageFunction = (response, excludePlaces, params) => ({
   results: mapSearchResultsToEntities(response, 20).filter(
-    result => !existingEntities.map(entity => entity.id).includes(result)
+    result => !excludePlaces.map(entity => entity.id).includes(result)
   ),
-  moreDataFunction: () =>
-    searchWithParams(existingEntities, {
-      ...params,
-      pagetoken: response.next_page_token
-    })
+  moreDataFunction: async () =>
+    getResultsWithNextPageFunction(
+      await doRequest('textsearch', {
+        ...params,
+        pagetoken: response.next_page_token
+      }),
+      excludePlaces,
+      params
+    )
 });
 
 // Response example:
-// https://maps.googleapis.com/maps/api/place/textsearch/json?key=API_KEY_HERE&location=32.0853%2C34.7818&type=restaurant&pagetoken=CpQCBwEAAGjYNql6iKPC2kOaCtS4Lc4tyj42ak9WZy2h6isCJWEUY9TQUpAh1Vwf35M0MC2qf9l1zJVgrnEqIt5GPYaM3dw2EiwgM1EEr--upS0FoKvv94jN3ACJVYTmrgA0SzguL-Np4AkJ704ngw1wKfdMaWvbvjVEiBlA3OUtPY3-zrichl-JaMerl0d_1FHzyRnyxBYWuKEEG-3S6FjaQOh-0Ks4lekJZiYW6NhD2QX3OVDFMNlpGs-88j-_3KT4AnUqzoC6KjUnE4GCY2-BdTGPW6hmNCXq2XIYtpsQ3xquT7iJb_C5N1lOSZebR-DLbwrDS7zkw_yDgapqr-yoeqVK0Jt9tBEeHGups2fnaovL1CEkEhDfhtVHsmK1rSaqt9sHrELRGhToqlUePY-ABMZgIprHyJtxaKTFNg
-const searchWithParams = async (existingEntities, params) =>
-  getResultsWithNextPageFunction(
-    await doRequest('textsearch', params),
-    existingEntities,
-    params
-  );
-
-// Response example:
 // https://maps.googleapis.com/maps/api/place/textsearch/json?key=API_KEY&location=32.0853%2C34.7818&type=restaurant
-exports.searchWithoutQuery = async existingEntities => {
+exports.searchWithoutQuery = async excludePlaces => {
   const location = getLocationParam();
+  const results = await innerSearchPlace('', location);
   const ret = getResultsWithNextPageFunction(
-    await innerSearchPlace('', location),
-    existingEntities,
+    results,
+    excludePlaces,
     { type: 'restaurant', location }
   );
-  ret.results = ret.results.map(result =>
-    Object.assign(result, { subtitle: 'Near you' })
-  );
+  ret.results = ret.results.map(result => ({
+    ...result,
+    subtitle: 'Near you'
+  }));
   return ret;
 };
