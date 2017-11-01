61004df46e8ed8f92b5b05e064aa7c56f38e76fd
itaibs <itaibs@gmail.com>
2017-11-01 20:40:49 +0200

    Update moviedb.js

--- moviedb.js
+++ moviedb.js
@@ -25,22 +25,55 @@ const bigPosterUrl = posterPath => getPosterUrl(posterPath, 'w500');
 
 const getBasicInfo = res => ({
   id: res.id,
-  description: res.overview,
   smallPictureSource: smallPosterUrl(res.backdrop_path),
   bigPictureSource: bigPosterUrl(res.backdrop_path),
   locationComplete: true
 });
 
+const getGenresString = (res, currentString) => {
+  const { genres } = res;
+  let ret = !!currentString ? ', ' : '';
+  if (genres && genres.length) {
+    ret += `${genres[0].name.toLowerCase()}`;
+    if (genres.length > 1) {
+      ret += `, ${genres[1].name.toLowerCase()}`;
+    }
+  }
+  return ret;
+};
+
+const getMovieDescription = res => {
+  let ret = '';
+  const { runtime } = res;
+  if (runtime) {
+    ret = `${runtime} minutes`;
+  }
+  ret += getGenresString(res, ret);
+  return ret;
+};
+
 const getBasicMovieInfo = res => ({
   ...getBasicInfo(res),
   title: res.title,
+  description: getMovieDescription(res),
   category: 'movies',
   link: res.imdb_id ? baseIMDBLinkUrl + res.imdb_id : ''
 });
 
+const getTvDescription = res => {
+  let ret = '';
+  const { episode_run_time } = res;
+  if (episode_run_time && episode_run_time.length) {
+    ret = `${episode_run_time[0]} minutes`;
+  }
+  ret += getGenresString(res, ret);
+  return ret;
+};
+
 const getBasicTvInfo = res => ({
   ...getBasicInfo(res),
   title: res.name,
+  description: getTvDescription(res),
   category: 'tv',
   link: baseTMDBLinkUrl + 'tv/' + res.id
 });
