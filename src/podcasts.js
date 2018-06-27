0e45193748efbc13a6df1f9bd1b9d908c4aa7da7
uri <uri.valevski@gmail.com>
2018-06-28 01:12:26 +0300

    podcasts

---
+++ src/podcasts.js
@@ -0,0 +1,62 @@
+const { fetch } = require('cross-fetch');
+const itunes = require('itunes-search');
+const unirest = require('unirest');
+
+const { makeCachedFunction } = require('./cache');
+const { makeRetryableFunction } = require('./retry');
+const { mashape } = require('./tokens');
+
+const podcastSearch = function(query) {
+  return new Promise((resolve, reject) => {
+    unirest
+      .get(
+        `https://listennotes.p.mashape.com/api/v1/search?q=${query}&type=episode`
+      )
+      .header('X-Mashape-Key', mashape)
+      .header('Accept', 'application/json')
+      .end(result => {
+        resolve(
+          result.body.results.map(x => ({
+            id: x.id,
+            title: x.title_original,
+            category: 'podcasts',
+            link: x.audio,
+            description: x.description_original,
+            bigPictureSource: x.image,
+            smallPictureSource: x.image
+          }))
+        );
+      });
+  });
+};
+
+exports.podcastSearch = makeCachedFunction(
+  makeRetryableFunction(podcastSearch, 'podcastSearch'),
+  'podcastSearch'
+);
+
+const podcastInfo = function(id) {
+  return new Promise((resolve, reject) => {
+    unirest
+      .get('https://listennotes.p.mashape.com/api/v1/episodes/' + id)
+      .header('X-Mashape-Key', mashape)
+      .header('Accept', 'application/json')
+      .end(result => {
+        const data = result.body;
+        resolve({
+          id: data.id,
+          title: data.title,
+          category: 'podcasts',
+          link: data.audio,
+          description: data.description,
+          bigPictureSource: data.podcast.image,
+          smallPictureSource: data.podcast.image
+        });
+      });
+  });
+};
+
+exports.podcastInfo = makeCachedFunction(
+  makeRetryableFunction(podcastInfo, 'podcastInfo'),
+  'podcastInfo'
+);
