3c0f7ca59a148107072aa02d39b0b3a3bb060a18
uri <uri.valevski@gmail.com>
2018-06-30 10:48:54 +0300

    remove unirest

--- src/podcasts.js
+++ src/podcasts.js
@@ -1,32 +1,29 @@
-const unirest = require('unirest');
 const h2p = require('html2plaintext');
 
 const { makeCachedFunction } = require('./cache');
 const { makeRetryableFunction } = require('./retry');
 const { tokens } = require('./tokens');
+const { fetch } = require('cross-fetch');
 
 const podcastSearch = function(query) {
-  return new Promise((resolve, reject) => {
-    unirest
-      .get(
-        `https://listennotes.p.mashape.com/api/v1/search?q=${query}&type=episode`
-      )
-      .header('X-Mashape-Key', tokens.mashape)
-      .header('Accept', 'application/json')
-      .end(result => {
-        resolve(
-          (result.body.results || []).map(x => ({
-            id: x.id,
-            title: x.title_original,
-            category: 'podcasts',
-            link: x.audio,
-            description: h2p(x.description_original),
-            bigPictureSource: x.image,
-            smallPictureSource: x.image
-          }))
-        );
-      });
-  });
+  return fetch(
+    `https://listennotes.p.mashape.com/api/v1/search?q=${query}&type=episode`,
+    {
+      headers: { 'X-Mashape-Key': tokens.mashape }
+    }
+  )
+    .then(response => response.json())
+    .then(responseJson =>
+      (responseJson.results || []).map(x => ({
+        id: x.id,
+        title: x.title_original,
+        category: 'podcasts',
+        link: x.audio,
+        description: h2p(x.description_original),
+        bigPictureSource: x.image,
+        smallPictureSource: x.image
+      }))
+    );
 };
 
 exports.podcastSearch = makeCachedFunction(
@@ -35,24 +32,19 @@ exports.podcastSearch = makeCachedFunction(
 );
 
 const podcastInfo = function(id) {
-  return new Promise((resolve, reject) => {
-    unirest
-      .get('https://listennotes.p.mashape.com/api/v1/episodes/' + id)
-      .header('X-Mashape-Key', tokens.mashape)
-      .header('Accept', 'application/json')
-      .end(result => {
-        const data = result.body;
-        resolve({
-          id: data.id,
-          title: data.title,
-          category: 'podcasts',
-          link: data.audio,
-          description: h2p(data.description),
-          bigPictureSource: data.podcast.image,
-          smallPictureSource: data.podcast.image
-        });
-      });
-  });
+  return fetch(`https://listennotes.p.mashape.com/api/v1/episodes/${id}`, {
+    headers: { 'X-Mashape-Key': tokens.mashape }
+  })
+    .then(response => response.json())
+    .then(data => ({
+      id: data.id,
+      title: data.title,
+      category: 'podcasts',
+      link: data.audio,
+      description: h2p(data.description),
+      bigPictureSource: data.podcast.image,
+      smallPictureSource: data.podcast.image
+    }));
 };
 
 exports.podcastInfo = makeCachedFunction(
